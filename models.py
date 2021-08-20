75310a80ac7434b9abad414c75bac7382fac6c97
Hunter Kiely <hunterkiely@hunters-mbp.lan>
2021-08-20 14:53:46 -0500

    Updated procfile

--- models.py
+++ models.py
@@ -30,5 +30,6 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
     def __repr__(self):
         return f'Item {self.name}'
